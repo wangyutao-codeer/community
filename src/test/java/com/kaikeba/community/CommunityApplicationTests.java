443c7ac610950a371898202b4b1f3d800032351c
wangyutao-codeer <=1178267844@qq.com>
2021-04-05 00:52:15 +0800

    init commit

---
+++ src/test/java/com/kaikeba/community/CommunityApplicationTests.java
@@ -0,0 +1,14 @@
+package com.kaikeba.community;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+ public class CommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
